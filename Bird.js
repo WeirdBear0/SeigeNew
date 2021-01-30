cc791744f3e22444ca50b968a4a22081c42b8ec3
WeirdBear0 <66538986+WeirdBear0@users.noreply.github.com>
2021-01-29 19:30:57 -0800

    Update Bird.js

--- Bird.js
+++ Bird.js
@@ -1,7 +1,7 @@
 class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/hexagon.png");
+    this.image = loadImage("hexagon.png");
   }
 
   display() {
