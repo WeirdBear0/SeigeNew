6740359316810a198cf4a073162557b7f701e0df
WeirdBear0 <66538986+WeirdBear0@users.noreply.github.com>
2021-01-29 19:30:18 -0800

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,7 +1,7 @@
 class Box extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/square2.png");
+    this.image = loadImage("square2.png");
     this.visibility = 255;
     this.counter = 0;
   }
